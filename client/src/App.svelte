3e6a285ff48e066e1d5210602776d584fad874b4
BartekStolarek <bartek.stolarek@gmail.com>
2022-04-13 16:14:30 +0200

    Implement main App Svelte component

---
+++ client/src/App.svelte
@@ -0,0 +1,37 @@
+<script>
+	import Header from './components/Header.svelte';
+	import Footer from './components/Footer.svelte';
+	import AdvertsList from './components/AdvertsList.svelte';
+
+	let adverts = [];
+
+	fetch('http://localhost:5000/api/v1/advertisements')
+		.then(response => response.json())
+		.then(advertisements => adverts = advertisements.data)
+
+	let filter = '';
+	let filteredAdverts = [];
+	let isFiltered = false;
+
+	$: filteredAdverts = adverts.filter(ad => {
+		let lowercasedTitle = ad.title.toLowerCase();
+		let lowercasedFilter = filter.toLowerCase();
+
+		return lowercasedTitle.includes(lowercasedFilter);
+	});
+	$: isFiltered = filter.length !== 0;
+
+	const clearFilters = () => {
+		filter = '';
+	};
+
+	const updateAdverts = (event) => {
+		adverts = event.detail.adverts.data;
+	};
+</script>
+
+<main>
+	<Header bind:filter={filter} on:updateAdverts={updateAdverts} />
+	<AdvertsList adverts={filteredAdverts} {isFiltered} on:clearFilters={clearFilters} />
+	<Footer />
+</main>
