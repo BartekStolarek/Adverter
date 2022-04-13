b3bdccb51ced160842f5d0a57f7341e694b9e69e
BartekStolarek <bartek.stolarek@gmail.com>
2022-04-13 16:13:54 +0200

    Implement CategoryIcon Svelte component

---
+++ client/src/components/CategoryIcon.svelte
@@ -0,0 +1,41 @@
+<script>
+  import Icon from '@iconify/svelte';
+
+  export let category = '';
+  let icon = '';
+
+  const assignIcon = (category) => {
+    switch (category) {
+      case 'ELECTRONICS':
+        icon = 'mdi:monitor-cellphone'
+        break;
+      case 'AUTOMOTIVE':
+        icon = 'mdi-car';
+        break;
+      case 'BOOKS':
+        icon = 'mdi:book-open-page-variant';
+        break;
+      case 'FASHION':
+        icon = 'mdi:shoe-sneaker';
+        break;
+      default:
+        icon = 'mdi:package-variant-closed';
+        break;
+    }
+  };
+
+  $: category && assignIcon(category);
+</script>
+
+<span class="category-icon">
+  <Icon icon={icon} width="50" height="50" />
+</span>
+
+<style>
+  .category-icon {
+    background-color: #ff986c;
+    color: white;
+    padding: 10px;
+    border-radius: 25px;
+  }
+</style>
