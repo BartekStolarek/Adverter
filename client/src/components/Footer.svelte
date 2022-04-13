84e4b86469af0e724398a6ae63ce7d23bddb441d
BartekStolarek <bartek.stolarek@gmail.com>
2022-04-13 16:13:42 +0200

    Implement static Svelte Footer component

---
+++ client/src/components/Footer.svelte
@@ -0,0 +1,21 @@
+<script>
+  import Icon from '@iconify/svelte';
+</script>
+
+<div class="footer">
+  Written in Svelte. 
+  <a href="https://github.com/BartekStolarek/clipzy" target="_blank">
+    See the source code on
+    <Icon class="icon" icon="mdi:github" width="25" height="25" />
+    Github
+  </a>
+</div>
+
+<style>
+  .footer {
+    margin: 20px;
+    padding: 30px;
+    border-top: 1px solid #ccc;
+    text-align: center;
+  }
+</style>
